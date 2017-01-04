537b1690d9e83c94abd0c8741813d9da87ccc758
Julianhey <julianhey@hotmail.com>
2017-01-04 23:24:24 +0100

    Added views for creating and editing todos

--- list/views.py
+++ list/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def todo_list(request):
     todos = Todo.objects.filter(
         deadline_date__lte=timezone.now()).order_by('deadline_date')
     return render(request, 'list/todo_list.html', {'todos': todos})
+
+def todo_detail(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    return render(request, 'list/todo_detail.html', {'todo': todo})
+
+def todo_new(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.author = request.user
+            todo.deadline_date = timezone.now()
+            todo.save()
+            return redirect('todo_detail', pk=todo.pk)
+    else:
+        form = TodoForm()
+    return render(request, 'list/todo_edit.html', {'form': form})
+
+def todo_edit(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.author = request.user
+            todo.deadline_date = timezone.now()
+            todo.save()
+            return redirect('todo_detail', pk=todo.pk)
+    else:
+        form = TodoForm(instance=post)
+    return render(request, 'list/todo_edit.html', {'form': form})
