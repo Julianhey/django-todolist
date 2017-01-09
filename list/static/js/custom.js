10bf321204de7111cc118fe3be00cf15f45038bb
Julianhey <julianhey@hotmail.com>
2017-01-09 22:17:03 +0100

    latest migrations

--- list/static/js/custom.js
+++ list/static/js/custom.js
@@ -1,14 +1,12 @@
-/**
- * Created by s157570 on 3-12-2016.
- */
 
 $(document).ready(function() {
-
-    var fieldparams = 'name,summary,first_release_date)'
+  var fieldparams = 'name,summary,first_release_date'
 
     $('.list-group-item').on('click', function () {
 
     var gamename = $(this).text();
+
+
 
     $.ajax({
     url: 'https://igdbcom-internet-game-database-v1.p.mashape.com/games/', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
@@ -17,10 +15,13 @@ $(document).ready(function() {
             'fields': fieldparams,
             'limit': 1}, // Additional parameters here
     datatype: 'json',
-    success: function(data) { $('#infopname').html(data[0].name)
-                              $('#infopsum').html(data[0].summary)
-                              $('#infopdate').html(data[0].first_release_date)},   //alert(JSON.stringify(data));
-    error: function(err) { alert(err); },
+    success: function(data) {
+    	$('#infopname').html(data[0].name)
+    	$('#infopsum').html(data[0].summary)
+      $('#infopdate').html(data[0].first_release_date)
+
+    },   //alert(JSON.stringify(data));
+    error: function(err) { alert(JSON.stringify(err)); },
     beforeSend: function(xhr) {
     xhr.setRequestHeader("X-Mashape-Authorization", "0oFh6CBgQDmsh4M7zsMvWXLtAfyap13eWAwjsnvo3Rxt6StIyb"); // Enter here your Mashape key
     }
